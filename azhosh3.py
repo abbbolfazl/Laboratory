641d19426a55e9cee856b9fa9c521c2dba3ac920
abolfazl <abolfazl@gmail.com>
2023-12-23 22:38:26 +0330

    “Message”

---
+++ azhosh3.py
@@ -0,0 +1,5 @@
+def detect_finger_joints(image):
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
+    mp_drawing = mp.solutions.drawing_utils
+    results = hands.process(image)
